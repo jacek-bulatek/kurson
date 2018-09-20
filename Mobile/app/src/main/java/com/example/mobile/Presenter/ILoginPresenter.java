aa14d7adbaa9ef8bbef5b216a780a2657fca60e5
jacek-bulatek <jacek.bulatek@gmail.com>
2018-09-20 16:42:29 +0200

    New Beggining

--- Mobile/app/src/main/java/com/example/mobile/Presenter/ILoginPresenter.java
+++ Mobile/app/src/main/java/com/example/mobile/Presenter/ILoginPresenter.java
@@ -1,9 +1,7 @@
 package com.example.mobile.Presenter;
-
 /**
  * Created by Jacek on 2018-09-19.
  */
-
 public interface ILoginPresenter {
     boolean setLogin(String login);
 }
--- Mobile/app/src/main/java/com/example/mobile/View/ILoginView.java
+++ Mobile/app/src/main/java/com/example/mobile/View/ILoginView.java
@@ -1,11 +1,8 @@
 package com.example.mobile.View;
-
 import android.view.View;
-
 /**
  * Created by Jacek on 2018-09-19.
  */
-
 public interface ILoginView {
     void displayLoginForm();
     void hideLoginFrom();
